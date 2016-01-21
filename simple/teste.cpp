e29f2c64e57122109b3959de72a357789b9a66b5
Felipe Magno de Almeida <felipe@expertisesolutions.com.br>
2016-01-21 14:27:06 -0200

    Simple1

--- simple/teste.cpp
+++ simple/teste.cpp
@@ -8,13 +8,17 @@ struct A;
 struct A {
   void clear() __attribute__(( pos(this->empty()) ));
   int size() const;
-  bool empty() const /* __attribute__(( pos( (this->size() == 0 && this->empty())
-                        || (this->size() != 0 && !this->empty())) ))*/;
+  bool empty() const;
   void push() __attribute__ (( pos( ! this->empty()) ));
   void pop()
-  //__attribute__ (( pre_constants( old_size(*this) == this->size()) ))
+    __attribute__ (( pre_constants( old_size(*this) == this->size()) ))
     __attribute__ (( pre( ! this->empty()) ))
-  //__attribute__ (( pos( this->size() == old_size(*this) + 1) ))
+    __attribute__ (( pos( this->size() == old_size(*this) + 1) ))
+    ;
+
+  void __invariants() const
+    __attribute__(( pre( (this->size() == 0) == this->empty()) ))
+    __attribute__(( pre( (this->size() != 0) == !this->empty()) ))
     ;
 };
 
--- simple/teste.v
+++ simple/teste.v
@@ -2,17 +2,25 @@
 Require Import Classical.
 
 Section teste.
-
   Variable A : Type.
-  Variable Empty : A -> Prop.
-  Variable Size: A -> nat.
-  Variable A_instance: A.
-  Variable N: nat.
-  Hypothesis A_instance_not_clear: (Size A_instance) <> 0.
-  Hypothesis Empty_if_size_is_zero_and_not_otherwise: ((Size A_instance) = 0 <-> (Empty A_instance)) /\ ((Size A_instance <> 0) <-> ~(Empty A_instance)).
+  Variable self : A.
+  Hypothesis rule1 : ~(empty self).
+  Theorem theorem : ~(empty self).
+  Qed.    
+
+(* Section teste. *)
+
+(*   Variable A : Type. *)
+(*   Variable Empty : A -> Prop. *)
+(*   Variable Size: A -> nat. *)
+(*   Variable A_instance: A. *)
+(*   Variable N: nat. *)
+(*   Hypothesis A_instance_not_clear: (Size A_instance) <> 0. *)
+(*   Hypothesis Empty_if_size_is_zero_and_not_otherwise: ((Size A_instance) = 0 <-> (Empty A_instance)) /\ ((Size A_instance <> 0) <-> ~(Empty A_instance)). *)
   
-  Theorem pop_is_valid: ~ (Empty A_instance).
-    intro.
-    tauto.
-  Qed.
+(*   Theorem pop_is_valid: ~ (Empty A_instance). *)
+(*     intro. *)
+(*     tauto. *)
+(*   Qed. *)
+
   
